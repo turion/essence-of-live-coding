ccf0bbaac5f85b3ea359e5eadc84d6e9e0008c18
Manuel Bärenz <m.baerenz@sonnen.de>
2019-08-17 15:47:21 +0200

    SQUASH essenc pulse ghci

---
+++ essenceoflivecoding-pulse/src/LiveCoding/Pulse/GHCi.hs
@@ -0,0 +1,29 @@
+module LiveCoding.Pulse.GHCi where
+
+-- base
+import Control.Concurrent
+
+-- foreign-store
+import Foreign.Store
+
+-- essenceoflivecoding
+import LiveCoding.LiveProgram
+
+-- essenceoflivecoding-pulse
+import LiveCoding.Pulse
+
+livepulse "" = livepulse "pulseCell"
+livepulse pulseCell = return $ unlines
+  [ "var <- playPulseCell " ++ pulseCell
+  , "savePulse var"
+  ]
+livereloadpulse "" = livereloadpulse "pulseCell"
+livereloadpulse pulseCell  = return $ unlines
+  [ ":reload"
+  , "var <- loadPulse"
+  , "updatePulse var " ++ pulseCell
+  ]
+loadPulse :: IO (MVar PulseCell)
+loadPulse = readStore (Store 0)
+savePulse :: MVar PulseCell -> IO ()
+savePulse var = writeStore (Store 0) var
