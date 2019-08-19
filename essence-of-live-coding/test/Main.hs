4c2b833935b20a2694e45c713902df80025367d2
Manuel Bärenz <m.baerenz@sonnen.de>
2019-08-19 11:12:06 +0200

    Transform test cases into QuickCheck cases

--- essence-of-live-coding/test/Main.hs
+++ essence-of-live-coding/test/Main.hs
@@ -1,31 +1,29 @@
 {-# LANGUAGE DeriveDataTypeable #-}
--- base
-import Data.Data
 
--- syb
-import Data.Generics.Aliases
-import qualified Data.Generics.Aliases as Aliases
-import Data.Generics.Twins
+-- test-framework
+import Test.Framework
+
+-- test-framework-quickcheck2
+import Test.Framework.Providers.QuickCheck2
 
 -- essence-of-live-coding
 import LiveCoding
 import qualified TestData.Foo1 as Foo1
 import qualified TestData.Foo2 as Foo2
 
-
 intToInteger :: Int -> Integer
 intToInteger = toInteger
 
-main = do
-  print $ 13 == migrate (10 :: Integer) (13 :: Integer)
-  print $ Foo1.foo' == migrate Foo1.foo Foo2.foo
-  print $ Foo2.foo' == migrate Foo2.foo Foo1.foo
-  print $ Foo2.bar' == migrate Foo2.bar Foo1.bar
-  print $ Foo2.baz' == migrate Foo2.baz Foo1.baz
-  print $ Foo2.frob' == migrateWith (userMigration intToInteger) Foo2.frob Foo1.frob
-  print $ Foo2.Frob 23 == migrate Foo2.frob (23 :: Integer)
-  print $ Debugging { dbgState = (42 :: Integer), state = (23 :: Integer) } == migrate Debugging { dbgState = (42 :: Integer), state = (42 :: Integer) } (23 :: Integer)
-  print $ (23 :: Integer) == migrate (5 :: Integer) Debugging { dbgState = (42 :: Integer), state = (23 :: Integer) }
--- TODO
--- * Make test suite assertions out of this
--- * Record tests
+main = defaultMain tests
+
+tests = return $ testGroup "simple tests"
+  [ testProperty "" $ 13 == migrate (10 :: Integer) (13 :: Integer)
+  , testProperty "" $ Foo1.foo' == migrate Foo1.foo Foo2.foo
+  , testProperty "" $ Foo2.foo' == migrate Foo2.foo Foo1.foo
+  , testProperty "" $ Foo2.bar' == migrate Foo2.bar Foo1.bar
+  , testProperty "" $ Foo2.baz' == migrate Foo2.baz Foo1.baz
+  , testProperty "" $ Foo2.frob' == migrateWith (userMigration intToInteger) Foo2.frob Foo1.frob
+  , testProperty "" $ Foo2.Frob 23 == migrate Foo2.frob (23 :: Integer)
+  , testProperty "" $ Debugging { dbgState = (42 :: Integer), state = (23 :: Integer) } == migrate Debugging { dbgState = (42 :: Integer), state = (42 :: Integer) } (23 :: Integer)
+  , testProperty "" $ (23 :: Integer) == migrate (5 :: Integer) Debugging { dbgState = (42 :: Integer), state = (23 :: Integer) }
+  ]
