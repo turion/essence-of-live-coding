c95675707da8beb9d13bc1eacca30c8d999d41d3
Manuel Bärenz <m.baerenz@sonnen.de>
2020-08-20 14:28:14 +0200

    Add LiveCoding.Cell.Util.foldC

--- essence-of-live-coding/src/LiveCoding/Cell/Util.hs
+++ essence-of-live-coding/src/LiveCoding/Cell/Util.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE Arrows #-}
+{-# LANGUAGE RecordWildCards #-}
 module LiveCoding.Cell.Util where
 
 -- base
 import Control.Arrow
+import Data.Data (Data)
 
 -- essence-of-live-coding
 import LiveCoding.Cell
@@ -15,3 +17,12 @@ sumFrom n0 = feedback n0 $ proc (n, acc) -> returnA -< (acc, acc + n)
 -- | Count the number of ticks, starting at 0
 count :: Monad m => Cell m a Integer
 count = arr (const 1) >>> sumC
+
+-- | Accumulate all incoming data,
+--   using the given fold function and start value.
+--   For example, if @'foldC' f b@ receives inputs @a0@, @a1@,...
+--   it will output @b@, @f a0 b@, @f a1 $ f a0 b@, and so on.
+foldC :: (Data b, Monad m) => (a -> b -> b) -> b -> Cell m a b
+foldC step cellState = Cell { .. }
+  where
+    cellStep b a = let b' = step a b in return (b, b')
