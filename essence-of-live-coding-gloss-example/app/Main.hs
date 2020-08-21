89c9cbda8364902c933819ec16bf8998fb7db7a2
Manuel Bärenz <m.baerenz@sonnen.de>
2020-08-21 17:28:12 +0200

    Make gloss natively nonblocking

--- essence-of-live-coding-gloss-example/app/Main.hs
+++ essence-of-live-coding-gloss-example/app/Main.hs
@@ -34,7 +34,7 @@ main :: IO ()
 main = runHandlingStateT $ foreground liveProgram
 
 liveProgram :: LiveProgram (HandlingStateT IO)
-liveProgram = liveCell $ glossWrapC defaultSettings { debugEvents = True } glossCell
+liveProgram = liveCell $ glossWrapC defaultSettings { debugEvents = True } glossCell >>> arr (const ())
 
 glossCell' :: Cell PictureM () ()
 glossCell'
