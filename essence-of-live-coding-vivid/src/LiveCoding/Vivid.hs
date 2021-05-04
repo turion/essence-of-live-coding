545421d9b474785844de470d0af3748fc138f9a6
Manuel Bärenz <programming@manuelbaerenz.de>
2021-05-04 20:25:09 +0200

    FIXUP vivid

--- essence-of-live-coding-vivid/src/LiveCoding/Vivid.hs
+++ essence-of-live-coding-vivid/src/LiveCoding/Vivid.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
 module LiveCoding.Vivid where
 
 -- vivid
@@ -7,40 +9,36 @@ import Vivid
 
 -- essence-of-live-coding
 import LiveCoding.Handle
-import Data.Foldable (traverse_)
-
-vividHandle
-  :: (VividAction m, VarList params, Subset (InnerVars params) args)
-  => SynthDef args
-  -> params
-  -> Handle m (Synth args)
-vividHandle synthDef params = Handle
-  { create  = synth synthDef params
-  , destroy = free
-  }
-
-vividHandleRelease
-  :: (VividAction m, VarList params, Elem "gate" args, Subset (InnerVars params) args)
-  => SynthDef args
-  -> params
-  -> Handle m (Synth args)
-vividHandleRelease synthDef params = Handle
-  { create  = synth synthDef params
-  , destroy = release
-  }
+import LiveCoding
 
 data SynthState
-  = NotYetStarted
-  | Started
-  | Released
+  = Started
+  | Stopped
+  deriving (Eq)
 
+-- fixme release instead of free?
 vividHandleParametrised
-  :: (VividAction m, VarList params, Subset (InnerVars params) args)
-  => SynthDef args
-  -> ParametrisedHandle m (params, SynthState) (Maybe (Synth args))
-vividHandleParametrised synthDef = ParametrisedHandle { .. }
+  :: (VividAction m, Eq params, VarList params, Subset (InnerVars params) args)
+  => ParametrisedHandle m (params, SynthDef args, SynthState) (Maybe (Synth args))
+vividHandleParametrised = ParametrisedHandle { .. }
   where
-    createParametrised (params, NotYetStarted) = defineSD synthDef >> pure Nothing
-    createParametrised (params, Started) = Just <$> synth synthDef params
-    createParametrised (params, Released) = pure Nothing
+    createParametrised (params, synthDef, Started) = Just <$> synth synthDef params
+    createParametrised (params, synthDef, Stopped) = defineSD synthDef >> pure Nothing
+
     destroyParametrised _ synthMaybe = traverse_ free synthMaybe
+
+    -- Only the synth parameters changed and it's still running.
+    -- So simply set new parameters without stopping it.
+    changeParametrised (paramsOld, synthDefOld, Started) (paramsNew, synthDefNew, Started) (Just synth)
+      | paramsOld /= paramsNew && synthDefOld == synthDefNew = do
+          set synth paramsNew
+          return $ Just synth
+    -- Synthdef or start/stop state changed, need to release and reinitialise
+    changeParametrised old new synth = defaultChange createParametrised destroyParametrised old new synth
+
+deriving instance Eq (SynthDef args)
+
+liveSynth
+  :: (VividAction m, VarList params, Subset (InnerVars params) args, Typeable args, Data params, Eq params, VarList (Synth args))
+  => Cell (HandlingStateT m) (params, SynthDef args, SynthState) (Maybe (Synth args))
+liveSynth = handlingParametrised vividHandleParametrised
