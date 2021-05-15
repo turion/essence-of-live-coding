57ab2fc39263109e0a52db264665d2a768516482
Manuel Bärenz <programming@manuelbaerenz.de>
2021-05-15 17:11:39 +0200

    Add integration test for handles

---
+++ essence-of-live-coding-ghci-example/app/Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+-- base
+import Control.Concurrent (threadDelay)
+
+-- essence-of-live-coding
+import LiveCoding
+
+liveProgram :: LiveProgram (HandlingStateT IO)
+liveProgram = liveCell $ handling Handle
+  { create = threadDelay 10000 >> putStrLn "Creating"
+  , destroy = const $ putStrLn "Destroying"
+  }
+
+main :: IO ()
+main = liveMain liveProgram
--- test-repl.sh
+++ test-repl.sh
@@ -19,6 +19,18 @@ fi
 rm result.txt
 }
 
+test_for_handle_messages() {
+# See essence-of-live-coding-ghci-example/app/Main.hs
+if grep -qE "Creating" result.txt && grep -qE "Destroying" result.txt
+then
+  rm result.txt
+else
+  cat result.txt
+  rm result.txt
+  exit 1
+fi
+}
+
 pushd gears
 test_repl
 popd
@@ -29,4 +41,9 @@ popd
 
 pushd essence-of-live-coding-pulse-example
 test_repl
+popd
+
+pushd essence-of-live-coding-ghci-example
+start_repl
+test_for_handle_messages
 popd
