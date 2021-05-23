8a4b16abd4b24d5c4a3ad20ac6c5d49edd8284ae
Manuel Bärenz <programming@manuelbaerenz.de>
2021-05-23 13:24:26 +0200

    FIXUP rework portmidi

--- essence-of-live-coding-PortMidi/src/LiveCoding/PortMidi.hs
+++ essence-of-live-coding-PortMidi/src/LiveCoding/PortMidi.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Arrows #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -15,6 +16,8 @@ module LiveCoding.PortMidi where
 import Control.Monad (void, forM, join)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Either (fromRight)
+import Data.Foldable (traverse_)
+import Data.Function ((&))
 import Data.Maybe (catMaybes)
 import GHC.Generics
 import GHC.TypeLits (Symbol, symbolVal, KnownSymbol)
@@ -27,40 +30,51 @@ import Sound.PortMidi
 
 -- essence-of-live-coding
 import LiveCoding
-import LiveCoding.LiveProgram.Except (foreverCLiveProgram)
-import qualified LiveCoding.LiveProgram.Except as LiveProgram
+
+data EOLCPortMidiError
+  = PMError PMError
+  | NoSuchDevice
+  | NotAnInputDevice
+  | NotAnOutputDevice
+  deriving (Data, Generic, Show)
+
+instance Finite EOLCPortMidiError
 
 -- | The monad transformer of PortMidi exceptions
-newtype PortMidiT m a = PortMidiT { unPortMidi :: ExceptT PMError m a }
-  deriving (Functor, Applicative, Monad)
+newtype PortMidiT m a = PortMidiT { unPortMidi :: ExceptT EOLCPortMidiError m a }
+  deriving (Functor, Applicative, Monad, MonadIO)
+
+throwPortMidi :: Monad m => EOLCPortMidiError -> PortMidiT m arbitrary
+throwPortMidi = PortMidiT . throwE
+
+throwPortMidiC :: Monad m => Cell (PortMidiT m) EOLCPortMidiError arbitrary
+throwPortMidiC = arrM throwPortMidi
 
 instance MonadTrans PortMidiT where
   lift = PortMidiT . lift
 
--- | Initialize the MIDI system, run the action, and shut it down again.
-runPortMidiT :: MonadIO m => PortMidiT m a -> ExceptT PMError m a
-runPortMidiT PortMidiT { .. } = ExceptT $ do
-  liftIO initialize
-  a <- runExceptT unPortMidi
-  liftIO $ void terminate
-  return a
+liftPMError :: Functor m => m (Either PMError a) -> PortMidiT m a
+liftPMError = PortMidiT . ExceptT . fmap (left PMError)
 
-instance (Launchable m, MonadIO m) => Launchable (PortMidiT m) where
-  runIO liveProgram = runIO $ foreverCLiveProgram $ do
-    e <- LiveProgram.try $ hoistLiveProgram unPortMidi liveProgram
-    once $ liftIO $ do
-      putStrLn "Encountered PortMidi exception:"
-      print e
-    return e
-    where
-      -- Will be part of essence-of-live-coding-0.2.6
-      once :: (Monad m, Data e, Finite e) => m e -> LiveProgram.LiveProgramExcept m e
-      once action = LiveProgram.try $ liveCell $ constM $ ExceptT $ Left <$> action
+-- | Initialize the MIDI system, run the action, and shut it down again.
+runPortMidiC :: MonadIO m => Cell (PortMidiT m) a b -> CellExcept (HandlingStateT m) a b EOLCPortMidiError
+runPortMidiC cell = try $ proc a -> do
+  _ <- liftCell $ handling portMidiHandle -< ()
+  hoistCell (mapExceptT lift . unPortMidi) cell -< a
+
+loopPortMidiC :: MonadIO m => Cell (PortMidiT m) a b -> Cell (HandlingStateT m) a b
+loopPortMidiC cell = foreverC $ runCellExcept $ do
+  e <- runPortMidiC cell
+  once_ $ liftIO $ do
+    putStrLn "Encountered PortMidi exception:"
+    print e
+  return e
 
 deriving instance Data PMError
 deriving instance Generic PMError
 instance Finite PMError
 
+-- FIXME do we still need this?
 -- | A marker to make sure that PortMidi was initialized
 data PortMidiHandle = PortMidiHandle
 
@@ -72,36 +86,69 @@ portMidiHandle = Handle
   , destroy = const $ liftIO $ void terminate
   }
 
-newtype PortMidiInputStream (name :: Symbol) = PortMidiInputStream { unPortMidiInputStream :: PMStream }
+newtype PortMidiInputStream = PortMidiInputStream { unPortMidiInputStream :: PMStream }
+newtype PortMidiOutputStream = PortMidiOutputStream { unPortMidiOutputStream :: PMStream }
 
-portMidiInputStreamAtProxyName :: Proxy name -> PMStream -> PortMidiInputStream name
-portMidiInputStreamAtProxyName _ = PortMidiInputStream
+lookupDeviceID :: MonadIO m => String -> (DeviceInfo -> Maybe EOLCPortMidiError) -> PortMidiT m DeviceID
+lookupDeviceID nameLookingFor filter = do
+  nDevices <- liftIO countDevices
+  devices <- forM [0..nDevices-1] $ \deviceID -> do
+    deviceInfo <- liftIO $ getDeviceInfo deviceID
+    traverse_ throwPortMidi $ filter deviceInfo
+    return (name deviceInfo, deviceID)
+  maybe (throwPortMidi NoSuchDevice) return $ lookup nameLookingFor devices
 
-portMidiInput
-  :: (MonadIO m, KnownSymbol name)
-  => PortMidiHandle
-  -> Handle m (Maybe (PortMidiInputStream (name :: Symbol)))
-portMidiInput _ = portMidiInputWithProxy Proxy
-
-portMidiInputWithProxy
-  :: (MonadIO m, KnownSymbol name)
-  => Proxy (name :: Symbol)
-  -> Handle m (Maybe (PortMidiInputStream (name :: Symbol)))
-portMidiInputWithProxy proxy = Handle
-  { create = liftIO $ do
-      nDevices <- countDevices
-      devices <- forM [0..nDevices-1] $ \deviceID -> do
-        deviceInfo <- getDeviceInfo deviceID
-        return $ if input deviceInfo then Just (name deviceInfo, deviceID) else Nothing
-      let deviceID = lookup (symbolVal proxy) $ catMaybes devices
-      pmStreamE <- mapM openInput deviceID
-      let pmStream = either (const Nothing) Just =<< pmStreamE
-      return $ portMidiInputStreamAtProxyName proxy <$> pmStream
-  , destroy = maybe (return ()) $ liftIO . void . close . unPortMidiInputStream
+portMidiInputStreamHandle
+  :: MonadIO m
+  => String
+  -> Handle (PortMidiT m) PortMidiInputStream
+portMidiInputStreamHandle name = Handle
+  { create = do
+      deviceID <- lookupDeviceID name $ \DeviceInfo { .. } -> if input then Nothing else Just NotAnInputDevice
+      PortMidiInputStream <$> liftPMError (liftIO (openInput deviceID))
+  , destroy = liftIO . void . close . unPortMidiInputStream
   }
 
-handlingPortMidiInput :: (KnownSymbol name, MonadIO m) => Cell (HandlingStateT m) PortMidiHandle (Maybe (PortMidiInputStream (name :: Symbol)))
-handlingPortMidiInput = handling $ portMidiInputWithProxy Proxy
+readEventsFrom
+  :: MonadIO m
+  => Cell (PortMidiT m) PortMidiInputStream [PMEvent]
+readEventsFrom = arrM $ liftPMError . liftIO . readEvents . unPortMidiInputStream
+
+readEventsC
+  :: MonadIO m
+  => String -> Cell (HandlingStateT (PortMidiT m)) arbitrary [PMEvent]
+readEventsC name = proc _ -> do
+  pmstream <- handling $ portMidiInputStreamHandle name -< ()
+  liftCell readEventsFrom -< pmstream
+
+portMidiOutputStreamHandle
+  :: MonadIO m
+  => String
+  -> Handle (PortMidiT m) PortMidiOutputStream
+portMidiOutputStreamHandle name = Handle
+  { create = do
+      deviceID <- lookupDeviceID name $ \DeviceInfo { .. } -> if output then Nothing else Just NotAnOutputDevice
+      -- Choose same latency as supercollider, see https://github.com/supercollider/supercollider/blob/18c4aad363c49f29e866f884f5ac5bd35969d828/lang/LangPrimSource/SC_PortMIDI.cpp#L416
+      -- Thanks Miguel Negrão
+      PortMidiOutputStream <$> liftPMError (liftIO (openOutput deviceID 0))
+  , destroy = liftIO . void . close . unPortMidiOutputStream
+  }
+
+writeEventsTo
+  :: MonadIO m
+  => Cell (PortMidiT m) (PortMidiOutputStream, [PMEvent]) ()
+writeEventsTo = arrM writer
+  where
+    writer (PortMidiOutputStream { .. }, events) = writeEvents unPortMidiOutputStream events
+      & liftIO
+      & liftPMError
+      & void
+
+writeEventsC
+  :: String -> Cell (HandlingStateT (PortMidiT IO)) [PMEvent] ()
+writeEventsC name = proc events -> do
+  portMidiOutputStream <- handling (portMidiOutputStreamHandle name) -< ()
+  liftCell writeEventsTo -< (portMidiOutputStream, events)
 
 data PortMidiDevices = PortMidiDevices
   { inputDevices :: [DeviceInfo]
